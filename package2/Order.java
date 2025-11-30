61c345182c00ad952589a430f96ea400c3435d9e
Joudkja1 <Joudkja1@gmail.com>
2025-11-30 21:44:41 +0300

    Update Order.java

--- package2/Order.java
+++ package2/Order.java
@@ -1,67 +1,68 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Order {
-    private int orderId;
-    private int customerReference;
-    private AVLTree<Integer, Product> products = new AVLTree<>();
-    private double total_price;
-    private LocalDate date;
-    private String status;
+    int orderId;
+    int customerRefrence;
+    AVLTree<Integer, Integer> products = new AVLTree<Integer, Integer>();
+    double total_price;
+    LocalDate date;
+    String status;
 
     public Order() {
         this.orderId = 0;
-        this.customerReference = 0;
+        this.customerRefrence = 0;
         this.total_price = 0;
         this.status = "";
-        this.date = LocalDate.now();
     }
 
-    public Order(int orderId, int customerReference, Product[] productList, double total_price, String date, String status) {
+    public Order(int orderId, int customerRefrence, Integer[] pids, double total_price, String date, String status) {
         this.orderId = orderId;
-        this.customerReference = customerReference;
+        this.customerRefrence = customerRefrence;
         this.total_price = total_price;
         this.date = LocalDate.parse(date);
         this.status = status;
 
-        for (Product p : productList) {
-            products.insert(p.getProductId(), p);
-        }
+        for (int i = 0; i < pids.length; i++)
+            products.insert(pids[i], pids[i]);
     }
 
-   
     public int getOrderId() { return orderId; }
-    public int getCustomerReference() { return customerReference; }
-    public AVLTree<Integer, Product> getProducts() { return products; }
+    public int getCustomerRefrence() { return customerRefrence; }
+    public AVLTree<Integer, Integer> getProducts() { return products; }
     public double getTotal_price() { return total_price; }
     public LocalDate getDate() { return date; }
     public String getStatus() { return status; }
 
-  
     public void setStatus(String status) { this.status = status; }
     public void setDate(LocalDate date) { this.date = date; }
 
-   
-    public void addProduct(Product p) {
-        products.insert(p.getProductId(), p);
+    public void addProduct(Integer product) {
+        products.insert(product, product);
     }
 
-    public boolean removeProduct(int pid) {
-        return products.removeKey(pid);
+    public boolean removeProduct(Integer p) {
+        return products.removeKey(p);
     }
 
-    public boolean isBetween(LocalDate start, LocalDate end) {
+    public boolean isDateBetween(LocalDate start, LocalDate end) {
         return (!date.isBefore(start) && !date.isAfter(end));
     }
 
     @Override
     public String toString() {
-        String s = "\nOrder { ID=" + orderId +
-                   ", Customer=" + customerReference +
-                   ", Total=" + total_price +
-                   ", Status=" + status +
-                   ", Date=" + date +
-                   ", Products=" + products.toString() +
-                   " }";
-        return s;
+        String str = "\nOrder{" + "orderId=" + orderId +
+                ", customer Refrence=" + customerRefrence +
+                ", total price=" + total_price +
+                " , status =" + status +
+                ", date =" + date;
+
+        if (!products.empty()) {
+            str += "( products List";
+            str += products;
+            str += " )";
+        }
+        str += " }";
+        return str;
     }
 }
